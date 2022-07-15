c5cf8cdb210d27b62f9554536fbab6f8fa8717fe
hamed6 <h.ghodsinezhad@gmail.com>
2022-07-15 13:57:08 +0300

    dockerizing jupyter script

--- 03-ingest-script/check_logfile.py
+++ 03-ingest-script/check_logfile.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
-
 import argparse
 import os
 from time import time
 
 import pandas as pd
-import pyarrow.parquet as pq
 from sqlalchemy import create_engine
 
 def main(user_args):
@@ -19,21 +17,24 @@ def main(user_args):
     url= user_args.url
     file_name= 'sample.csv'
     
-    os.system(f"wget {url} -O {file_name}")
+    # os.system(f"wget {url} -O {file_name}")
     
 
     engine=create_engine(f'postgresql://{user_name}:{password}@{host}:{port}/{db_name}')
     engine.connect()
 
-    df_iterate=pd.read_csv('./taxi_data.csv', iterator=True, chunksize=100000)
+    
+    df_iterate=pd.read_csv(url, iterator=True, chunksize=100000)
+    df=next(df_iterate)
 
     # Insert only column names of data to the db.
-    df.head(n=0).to_sql(name='taxi_data', con=engine, if_exists='replace')
+    df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
+
 
     while True:
         # Get the next batch of csv file
         df=next(df_iterate)    
-        df.to_sql(name='taxi_data', con=engine, if_exists='append')
+        df.to_sql(name=table_name, con=engine, if_exists='append')
         print(time())
 
 if __name__=="__main__":
