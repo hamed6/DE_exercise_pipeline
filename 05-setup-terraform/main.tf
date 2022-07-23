df21cd869119cd68c075a610e959260ac859afb0
hamed6 <h.ghodsinezhad@gmail.com>
2022-07-23 19:13:40 +0300

    add: terraform resources

--- 05-setup-terraform/main.tf
+++ 05-setup-terraform/main.tf
@@ -11,9 +11,26 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "aws_vpc" "de-vpc" {
+  cidr_block = "10.0.0.0/26"
+
+  tags = {
+    "name" = "de-vpc-01"
+  }
+}
+
+resource "aws_default_subnet" "de-subnet" {
+  
+  availability_zone = "us-east-1a"
+
+  tags = {
+    "name" = "de-subnet-v01"
+  }
+}
 
 resource "aws_s3_bucket" "de-bucket" {
   bucket = "de-s3-bucket-v01"
+
   tags = {
     "name" = "de-s3"
   }
@@ -31,4 +48,8 @@ resource "aws_redshift_cluster" "de-redshift" {
   master_password = var.database_credential_pass
   node_type = var.node_type
   cluster_type = var.cluster_type
+
+  tags = {
+    "name" = "de-redshift-v01"
+  }
 }
