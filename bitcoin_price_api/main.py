eb2ff9e04b887d24279b5e8ccd7bc1eb4e0e9cf9
hamed6 <h.ghodsinezhad@gmail.com>
2023-02-19 20:37:28 +0200

    upd: update date format

--- bitcoin_price_api/main.py
+++ bitcoin_price_api/main.py
@@ -2,31 +2,34 @@ import requests
 import sqlite3
 from datetime import datetime
 
-
 def call_api():
     """call the free api to get bitcoin details"""
         
     api_response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
     full_response=api_response.json()
+
     return full_response
 
 
 def extract_from_api():
-    
+    """extract date and rate"""
     full_response=call_api()
     get_coin_date = full_response['time']['updated']
-    get_coin_date=datetime.strptime(get_coin_date, "%m %d, %y %H:%M:%S")
-    get_coin_rate = full_response['bpi']['USD']['rate']
+    get_coin_rate = full_response['bpi']['USD']['rate_float']
+    # convert date to db format
+    get_coin_date=datetime.strptime(get_coin_date, "%b %d, %Y %H:%M:%S %Z")
+    get_coin_date=datetime.strftime(get_coin_date, "%Y%M%d")
+    
     return get_coin_date, get_coin_rate
 
 def load_data():
-
+    """connect to db and save data"""
     get_coin_date,get_coin_rate=extract_from_api() 
     
-    insert_sql=f"INSERT INTO bitcoin_daily_rate VALUES ({get_coin_date})" 
+    insert_sql=f"INSERT INTO bitcoin_daily_rate VALUES ({get_coin_date}, {get_coin_rate})" 
     con=sqlite3.connect("bicoin_price_db.db")
     cursor=con.cursor()
-    cursor.execute(f"INSERT INTO bitcoin_daily_rate VALUES ({get_coin_date}, {get_coin_rate})")
+    cursor.execute(insert_sql)
     con.commit()
 
 
