33f1d7426ca5de89aa261564775642c60ae91bb3
hamed6 <h.ghodsinezhad@gmail.com>
2023-02-23 18:58:20 +0200

    upd: pagination model

--- python_pagination_api/data_models.py
+++ python_pagination_api/data_models.py
@@ -2,9 +2,6 @@ from dataclasses import dataclass
 from typing import Literal, Dict, List, Optional
 
 
-    
-
-
 
 @dataclass
 class CharacterSchema:
@@ -25,16 +22,12 @@ class CharacterSchema:
     url: str
     created: str
 
-
-
 class InfoSchema:
     def __init__(self, count: int, 
                  pages: int, 
                  next: Optional[str], 
                  prev: Optional[str] ) -> None:
         pass
-
-
 
 @dataclass
 class ResponseSchema:
@@ -44,3 +37,12 @@ class ResponseSchema:
     def __post_init__(self):
         self.info= InfoSchema(**self.info)
         self.results= [CharacterSchema(**x) for x in self.results]
+
+@dataclass
+class FilterApi:
+    page: Optional[str] = None
+    name: Optional[str] = None
+    status: Optional[Literal['unknown', 'Alive', 'Dead']] = None
+    species: Optional[str]=None
+    type: Optional[str] = None
+    gender: Optional[Literal['female', 'male', 'genderless', 'unknown']] = None
